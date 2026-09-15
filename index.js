7b6e6197b726e31c124300a52d2fad9edb7fce95
MonalKumar <monalshiv123@gmail.com>
2026-09-15 23:42:21 +0530

    faang level interviews

--- index.js
+++ index.js
@@ -2591,10 +2591,16 @@ function liveHappily(){
 // }
 
 
-const ingredients=()=>{
-    return new Promise((resolve)=>{
-        setTimeout(()=>{
-            console.log("hellow world")
-        })
-    })
-}
+console.log("1");
+
+setTimeout(() => console.log("2"), 0);
+
+Promise.resolve().then(() => console.log("3"));
+
+Promise.resolve().then(() => {
+  console.log("4");
+  setTimeout(() => console.log("5"), 0);
+  Promise.resolve().then(() => console.log("6"));
+});
+
+console.log("7");
